964bee96b0cd54b91afa331891f7b80bf931777a
Zen <74816455+ajfalcotelo@users.noreply.github.com>
2026-06-12 05:33:20 +0800

    refactor: move cellcenter logic to unit script
    removed obsolete PlayerController

--- Assets/Scripts/Units/EnemyUnit.cs
+++ Assets/Scripts/Units/EnemyUnit.cs
@@ -29,8 +29,11 @@ public class EnemyUnit : MonoBehaviour, IUnit
         currentRange = unitData.Range;
     }
 
-    // void Start()
-    // {
-    //     GridManager.Instance.GetNode(transform.position).Occupant = this;
-    // }
+    void Start()
+    {
+        GridManager.Instance.GetNode(transform.position).Occupant = this;
+        transform.position = GridManager.Instance.GetGroundTilemap.GetCellCenterWorld(
+            Vector3Int.RoundToInt(transform.position)
+        );
+    }
 }
