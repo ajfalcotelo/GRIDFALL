a0ec2242e73878a8d0e03f48c946d36a945f2b1c
Zen <74816455+ajfalcotelo@users.noreply.github.com>
2026-06-26 12:48:46 +0800

    refactor: make NodeDebug text configurable via inspector selection

--- Assets/Scripts/NodeDebug.cs
+++ Assets/Scripts/NodeDebug.cs
@@ -5,6 +5,15 @@ using UnityEngine.Tilemaps;
 
 public class NodeDebug : MonoBehaviour
 {
+    private enum DebugText
+    {
+        Unit,
+        Position,
+    }
+
+    [SerializeField]
+    private DebugText debugText;
+
     private Dictionary<Vector2Int, TextMeshPro> test;
     private Tilemap groundTilemap;
 
@@ -13,21 +22,18 @@ public class NodeDebug : MonoBehaviour
         groundTilemap = GridManager.Instance.GetGroundTilemap;
         test = new();
         for (int x = 0; x < groundTilemap.cellBounds.size.x; x++)
+        for (int y = 0; y < groundTilemap.cellBounds.size.y; y++)
         {
-            for (int y = 0; y < groundTilemap.cellBounds.size.y; y++)
-            {
-                var cell = new Vector2Int(x, y);
-                GameObject gameObject = new("World_Text", typeof(TextMeshPro));
-                Transform transform = gameObject.transform;
-                transform.SetParent(null, false);
-                transform.localPosition =
-                    GridManager.Instance.NodeToWorld(cell) + Vector2.one * 0.5f;
-                TextMeshPro textMesh = gameObject.GetComponent<TextMeshPro>();
-                textMesh.alignment = TextAlignmentOptions.Center;
-                textMesh.fontSize = 3;
-                textMesh.color = Color.white;
-                test.Add(cell, textMesh);
-            }
+            var cell = new Vector2Int(x, y);
+            GameObject gameObject = new("World_Text", typeof(TextMeshPro));
+            Transform transform = gameObject.transform;
+            transform.SetParent(null, false);
+            transform.localPosition = GridManager.Instance.NodeToWorld(cell) + Vector2.one * 0.5f;
+            TextMeshPro textMesh = gameObject.GetComponent<TextMeshPro>();
+            textMesh.alignment = TextAlignmentOptions.Center;
+            textMesh.fontSize = 3;
+            textMesh.color = Color.white;
+            test.Add(cell, textMesh);
         }
     }
 
@@ -37,10 +43,16 @@ public class NodeDebug : MonoBehaviour
         for (int y = 0; y < groundTilemap.cellBounds.size.y; y++)
         {
             var cell = new Vector2Int(x, y);
-            var node = GridManager.Instance.OccupancyLayer.GetNode(cell);
             if (test.TryGetValue(cell, out TextMeshPro value))
             {
-                value.text = $"{node}";
+                string text = debugText switch
+                {
+                    DebugText.Unit => $"{cell}",
+                    DebugText.Position => $"{GridManager.Instance.OccupancyLayer.GetNode(cell)}",
+                    _ => "",
+                };
+
+                value.text = text;
             }
         }
     }
