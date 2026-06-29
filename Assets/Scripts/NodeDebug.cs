b8187fe27bddb86176a418a6df08bd8646f537e1
Zen <74816455+ajfalcotelo@users.noreply.github.com>
2026-06-29 14:48:26 +0800

    feat: add method for custom debug text

--- Assets/Scripts/NodeDebug.cs
+++ Assets/Scripts/NodeDebug.cs
@@ -5,55 +5,83 @@ using UnityEngine.Tilemaps;
 
 public class NodeDebug : MonoBehaviour
 {
-    private enum DebugText
+    private enum DebugMode
     {
+        Custom,
         Unit,
         Position,
     }
 
-    [SerializeField]
-    private DebugText debugText;
+    public static NodeDebug Instance { get; private set; }
 
-    private Dictionary<Vector2Int, TextMeshPro> test;
+    [SerializeField]
+    private DebugMode debugMode;
+
+    [SerializeField]
+    private bool showDebug;
+
+    [SerializeField]
     private Tilemap groundTilemap;
+
+    private Dictionary<Vector2Int, TextMeshPro> text = new();
+    private Dictionary<Vector2Int, string> customText = new();
+    private Transform debugContainer;
+
+    void Awake()
+    {
+        Instance = this;
+
+        debugContainer = new GameObject("Debug_Container").transform;
+        debugContainer.SetParent(transform);
+    }
 
     void Start()
     {
-        groundTilemap = GridManager.Instance.GetGroundTilemap;
-        test = new();
         for (int x = 0; x < groundTilemap.cellBounds.size.x; x++)
         for (int y = 0; y < groundTilemap.cellBounds.size.y; y++)
         {
             var cell = new Vector2Int(x, y);
             GameObject gameObject = new("World_Text", typeof(TextMeshPro));
             Transform transform = gameObject.transform;
-            transform.SetParent(null, false);
-            transform.localPosition = GridManager.Instance.NodeToWorld(cell) + Vector2.one * 0.5f;
+            transform.SetParent(debugContainer, false);
+            transform.position = GridManager.Instance.NodeToWorld(cell) + Vector2.one * 0.5f;
             TextMeshPro textMesh = gameObject.GetComponent<TextMeshPro>();
             textMesh.alignment = TextAlignmentOptions.Center;
             textMesh.fontSize = 3;
             textMesh.color = Color.white;
-            test.Add(cell, textMesh);
+            text.Add(cell, textMesh);
         }
     }
 
     void Update()
     {
-        for (int x = 0; x < groundTilemap.cellBounds.size.x; x++)
-        for (int y = 0; y < groundTilemap.cellBounds.size.y; y++)
+        if (showDebug)
         {
-            var cell = new Vector2Int(x, y);
-            if (test.TryGetValue(cell, out TextMeshPro value))
+            for (int x = 0; x < groundTilemap.cellBounds.size.x; x++)
+            for (int y = 0; y < groundTilemap.cellBounds.size.y; y++)
             {
-                string text = debugText switch
+                var cell = new Vector2Int(x, y);
+                if (text.TryGetValue(cell, out TextMeshPro value))
                 {
-                    DebugText.Unit => $"{cell}",
-                    DebugText.Position => $"{GridManager.Instance.OccupancyLayer.GetNode(cell)}",
-                    _ => "",
-                };
+                    string debugText = debugMode switch
+                    {
+                        DebugMode.Custom => customText.TryGetValue(cell, out string val) ? val : "",
+                        DebugMode.Unit => $"{cell}",
+                        DebugMode.Position =>
+                            $"{GridManager.Instance.OccupancyLayer.GetNode(cell)}",
+                        _ => "",
+                    };
 
-                value.text = text;
+                    value.text = debugText;
+                }
             }
         }
+    }
+
+    // call on scripts for custom texts
+    public void UpdateText(Dictionary<Vector2Int, string> text)
+    {
+        debugMode = DebugMode.Custom;
+        customText = text;
     }
 }
